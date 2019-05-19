87952258632af6924c70f87fd7202a7b070ebda0
doehm <danieloehm@gmail.com>
2019-05-19 18:33:26 +1000

    Changed UI and added forest plot panel

--- server.R
+++ server.R
@@ -9,6 +9,7 @@ library(htmltools)
 library(htmlwidgets)
 library(showtext)
 library(data.table)
+library(lazyeval)
 
 # load data
 load("./data/road-accident-data.Rdata")
@@ -26,12 +27,132 @@ my_theme <- function(){
       legend.position = "none",
       axis.text = element_text(family = "mont", size = 12),
       axis.text.x = element_text(angle = 45, vjust = 0.5),
+      axis.text.y = element_blank(),
       axis.title = element_blank(),
-      plot.title = element_text(family = "mont", hjust = 0.5, face = "bold")
+      plot.title = element_text(family = "mont", hjust = 0.5, face = "bold"),
+      plot.subtitle = element_text(family = "mont", hjust = 0.5, face = "bold")
     )
 }
 
 my_cols <- function(n = 16) colorRampPalette(c("darkmagenta", "turquoise"))(n)
+
+theme_forest <- function(scale = 1){
+  theme_minimal() +
+    theme(
+      legend.position = "none",
+      axis.text = element_text(family = "mont", size = 16*scale),
+      axis.text.x = element_text(vjust = 0.5),
+      axis.title.y = element_blank(),
+      axis.title.x = element_text(family = "mont", size = 16*scale),
+      plot.title = element_text(family = "mont", hjust = 0.5, size = 26*scale, face = "bold"),
+      plot.subtitle = element_text(family = "mont", hjust = 0.5, size = 18*scale),
+      plot.caption = element_text(size = 12*scale)
+    )
+}
+
+posterior_f <- function(df, y, n, a = 1.30, b = 77, inflator = 100) {
+  out <- data.frame()
+  qs <- c(0.025, 0.1, 0.5, 0.9, 0.975)
+  for(k in 1:nrow(df)){
+    out <- rbind(out, inflator*rbeta(1e4, shape1 = a+y[k], shape2 = b+n[k]-y[k]) %>% quantile(qs))
+  }
+  colnames(out) <- paste0("q", 100*qs)
+  return(out)
+}
+
+
+
+
+set.seed(190513)
+# fatalities
+areas <- grep("Area", colnames(accidents_raw), value = TRUE)[3:4]
+names(areas) <- c("sa3", "sa4")
+fatality_fn <- function(area){
+  accidents_raw %>% 
+    group_by_(area) %>% 
+    filter(
+      Crash_Year == 2017,
+    ) %>% 
+    summarise(
+      count = length(Count_Casualty_Total),
+      n_fatalities = sum(Count_Casualty_Fatality)
+    ) %>% 
+    bind_cols(posterior_f(df = ., y = .$n_fatalities, n = .$count)) %>% 
+    arrange(q50) %>% 
+    mutate_(area = interp(~factor(v, level = v), v = as.name(area))) %>% 
+    ggplot() +
+    geom_segment(mapping = aes(x = q2.5, xend = q97.5, y = area, yend = area)) +
+    geom_segment(mapping = aes(x = q10, xend = q90, y = area, yend = area, col = q50), size = 2) +
+    geom_point(mapping = aes(x = q50, y = area), pch = 3) +
+    theme_forest() +
+    scale_colour_gradientn(colors = my_cols()) +
+    labs(
+      title = "Fatality rate given observed road accidents",
+      subtitle = paste("Bayesian estimate of the fatality rate for", toupper(names(area)), "areas in 2017"),
+      x = "Fatality rate (%)")
+}
+fatality_plots <- list(sa3 = fatality_fn(areas[1]), sa4 = fatality_fn(areas[2]))
+
+
+
+# cyclists
+cyclist_fn <- function(area){
+  accidents_raw %>% 
+    group_by_(area) %>% 
+    filter(Crash_Year == 2017) %>% 
+    summarise(
+      count = n(),
+      n_bicycles = sum(Count_Unit_Bicycle > 0)
+    ) %>% 
+    bind_cols(posterior_f(df = ., y = .$n_bicycles, n = .$count, a = 1.55, b = 25)) %>% 
+    arrange(q50) %>% 
+    mutate_(area = interp(~factor(v, level = v), v = as.name(area))) %>% 
+    ggplot() +
+    geom_segment(mapping = aes(x = q2.5, xend = q97.5, y = area, yend = area)) +
+    geom_segment(mapping = aes(x = q10, xend = q90, y = area, yend = area, col = q50), size = 2) +
+    geom_point(mapping = aes(x = q50, y = area), pch = 3) +
+    theme_forest() +
+    scale_colour_gradientn(colors = my_cols()) +
+    labs(
+      title = "Rate of cyclists involved in road accidents",
+      subtitle = paste("Bayesian estimate of the rate of accidents involving cyclists for", toupper(names(area)), "areas in 2017"),
+      x = "Accidents involving cyclists (%)"
+    )
+}
+cyclist_plots <- list(sa3 = cyclist_fn(areas[1]), sa4 = cyclist_fn(areas[2]))
+
+
+# pedestrians
+pedestrian_fn <- function(area){
+  accidents_raw %>% 
+    group_by_(area) %>% 
+    filter(Crash_Year == 2017) %>% 
+    summarise(
+      count = n(),
+      n_bicycles = sum(Count_Unit_Pedestrian > 0)
+    ) %>% 
+    bind_cols(posterior_f(df = ., y = .$n_bicycles, n = .$count, a = 3, b = 60)) %>% 
+    arrange(q50) %>% 
+    mutate_(area = interp(~factor(v, level = v), v = as.name(area))) %>% 
+    ggplot() +
+    geom_segment(mapping = aes(x = q2.5, xend = q97.5, y = area, yend = area)) +
+    geom_segment(mapping = aes(x = q10, xend = q90, y = area, yend = area, col = q50), size = 2) +
+    geom_point(mapping = aes(x = q50, y = area), pch = 3) +
+    theme_forest() +
+    scale_colour_gradientn(colors = my_cols()) +
+    labs(
+      title = "Rate of pedestrians involved in road accidents",
+      subtitle = paste("Bayesian estimate of the rate of accidents involving pedestrians for", toupper(names(area)), "areas in 2017"),
+      x = "Pedestrians injured from the accident (%)"
+    )
+}
+pedestrian_plots <- list(sa3 = pedestrian_fn(areas[1]), sa4 = pedestrian_fn(areas[2]))
+
+
+
+
+
+
 
 function(input, output, session) {
   
@@ -42,15 +163,21 @@ function(input, output, session) {
       {if(input$loc == "Loc_ABS_Statistical_Area_4") filter(., Loc_ABS_Statistical_Area_4 == input$sa4) else .} %>% 
       {if(input$loc == "Loc_Local_Government_Area") filter(., Loc_Local_Government_Area == input$lga) else .} %>% 
       {if(input$loc == "Loc_ABS_Remoteness") filter(., Loc_ABS_Remoteness == input$remote) else .} %>% 
-      {if(input$day_of_week != "...") filter(., Crash_Day_Of_Week == input$day_of_week) else .} %>% 
-      {if(input$month != "...") filter(., Crash_Month == input$month) else .} %>% 
-      {if(input$weather != "...") filter(., Crash_Atmospheric_Condition == input$weather) else .} %>% 
-      {if(input$driving_conditions != "...") filter(., Crash_Lighting_Condition == input$driving_conditions) else .} %>% 
-      {if(input$road_condition != "...") filter(., Crash_Road_Surface_Condition == input$road_condition) else .} %>% 
-      {if(input$speed_limit != "...") filter(., Crash_Speed_Limit == input$speed_limit) else .} %>% 
-      {if(input$road_feature != "...") filter(., Crash_Roadway_Feature == input$road_feature) else .} %>% 
-      {if(input$crash_type != "...") filter(., Crash_Nature == input$crash_type) else .} %>% 
-      {if(input$crash_severity != "...") filter(., Crash_Severity == input$crash_severity) else .} %>% 
+      {if(input$unit == "Bicycle") filter(., Count_Unit_Bicycle > 0) else .} %>% 
+      {if(input$unit == "Car") filter(., Count_Unit_Car > 0) else .} %>% 
+      {if(input$unit == "Motocycle") filter(., Count_Unit_Motorcycle_Moped > 0) else .} %>% 
+      {if(input$unit == "Truck") filter(., Count_Unit_Truck > 0) else .} %>% 
+      {if(input$unit == "Pedestrian") filter(., Count_Unit_Pedestrian > 0) else .} %>% 
+      {if(input$unit == "Bus") filter(., Count_Unit_Bus > 0) else .} %>% 
+      {if(input$day_of_week != "..." & !is.null(input$day_of_the_week)) filter(., Crash_Day_Of_Week == input$day_of_week) else .} %>% 
+      {if(input$month != "..." & !is.null(input$month)) filter(., Crash_Month == input$month) else .} %>% 
+      {if(input$weather != "..." & !is.null(input$weather)) filter(., Crash_Atmospheric_Condition == input$weather) else .} %>% 
+      {if(input$driving_conditions != "..." & !is.null(input$driving_conditions)) filter(., Crash_Lighting_Condition == input$driving_conditions) else .} %>% 
+      {if(input$road_condition != "..." & !is.null(input$road_condition)) filter(., Crash_Road_Surface_Condition == input$road_condition) else .} %>% 
+      {if(input$speed_limit != "..." & !is.null(input$speed_limit)) filter(., Crash_Speed_Limit == input$speed_limit) else .} %>% 
+      {if(input$road_feature != "..." & !is.null(input$road_feature)) filter(., Crash_Roadway_Feature == input$road_feature) else .} %>% 
+      {if(input$crash_type != "..." & !is.null(input$crash_type)) filter(., Crash_Nature == input$crash_type) else .} %>% 
+      {if(input$crash_severity != "..." & !is.null(input$crash_severity)) filter(., Crash_Severity == input$crash_severity) else .} %>% 
       mutate(fatality = Count_Casualty_Fatality > 0)
   })
   
@@ -78,7 +205,7 @@ function(input, output, session) {
   
   # draws from the posterior
   fatalityRateSim <- reactive({
-    rgamma(1e4, shape = 1.33 + fatalityRate()$n_fatalities, rate = 56 + fatalityRate()$count)
+    rbeta(1e4, shape1 = 1.3 + fatalityRate()$n_fatalities, shape2 = 77 + fatalityRate()$count - fatalityRate()$n_fatalities)
   })
   
   # count of casualty type
@@ -141,11 +268,7 @@ function(input, output, session) {
       ggplot(aes(x = x)) +
       geom_histogram(fill = "turquoise") +
       my_theme() +
-      labs(title = "Rate of road accident fatalities\nper 100 crashes per year")
-  })
-  
-  output$rate_text <- renderText({
-    round(100*median(fatalityRateSim()), 1)
+      labs(title = paste0("Rate of road accident fatalities in 2017 (", round(100*median(fatalityRateSim()), 1), ")"))
   })
   
   # leaflet map
@@ -194,6 +317,27 @@ function(input, output, session) {
   
   output$crash_data <- renderDataTable({
     selectData()
-  }, options = list(pageLength = 20))
+  }, options = list(pageLength = 15))
   
+  # TODO: fix the height of this output - make it dynamic
+  # also just make this better - for now, path of least resistance
+  selectForestPlot <- reactive({
+    if(input$floc == "Loc_ABS_Statistical_Area_3"){
+      k <- 1
+    }else if(input$floc == "Loc_ABS_Statistical_Area_4"){
+      k <- 2
+    }
+    if(input$measure == "Fatality"){
+      fatality_plots[[k]]
+    }else if(input$measure == "Cyclists"){
+      cyclist_plots[[k]]
+    }else if(input$measure == "Pedestrians"){
+      pedestrian_plots[[k]]
+    }
+  })
+  
+  output$forest_plot <- renderPlot({
+    selectForestPlot()
+  }, width = 900, height = 1200)
+
 }
--- ui.R
+++ ui.R
@@ -9,10 +9,13 @@ library(data.table)
 load("./data/road-accident-data.Rdata")
 
 # set filter vars
+check_box <- c("Day of the week", "Month", "Weather conditions", "Unit type involved", "Driving conditions", "Speed limit", 
+               "Road feature", "Crash type", "Crash severity")
 year <- accidents_raw$Crash_Year %>% unique %>% sort
 day_of_week <- c("...", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 month <- c("...", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
 weather_conditions <- c("...", accidents_raw$Crash_Atmospheric_Condition %>% unique %>% sort)
+units <- c("...", "Bicycle", "Car", "Truck", "Motorcycle", "Pedestrian", "Bus")
 driving_conditions <- c("...", accidents_raw$Crash_Lighting_Condition %>% unique %>% sort)
 road_condition <- c("...", accidents_raw$Crash_Road_Surface_Condition %>% unique %>% sort)
 speed_limit <- c("...", accidents_raw$Crash_Speed_Limit %>% unique %>% sort)
@@ -41,13 +44,17 @@ navbarPage("Road accidents in Queensland", id="road",
                           id = "controls", class = "panel panel-default", fixed = TRUE,
                           draggable = TRUE, top = 90, left = "auto", right = 180, bottom = "auto",
                           width = 250, height = "auto",
-                          HTML('<button data-toggle="collapse" data-target="#demo">Show filters</button>'),
-                          tags$div(id = 'demo',  class="collapse",
+                          HTML('<button data-toggle="collapse" data-target="#main">Show filters</button>'),
+                          tags$div(id = 'main',  class="collapse",
+                                   
+                                   # this isn't exactly elegant but it's the path of least resistance
+                                   
+                                   # select year
+                                   sliderInput("year", label = "Year", min = min(year), max = max(year), value = c(2013, 2018), round = TRUE, step = 1),
                                    
                                    # select statistical area
+                                   # TODO: put better labels on these
                                    selectInput("loc", "Location category", loc_type, selected = "Loc_ABS_Statistical_Area_3"),
-                                   
-                                   #TODO: reduce all the conditional panels with a single uiOutput
                                    
                                    # if sa2 select area with sa2
                                    conditionalPanel(
@@ -79,17 +86,68 @@ navbarPage("Road accidents in Queensland", id="road",
                                      selectInput("remote", "Location", loc_list[["Loc_ABS_Remoteness"]], selected = "Major Cities")
                                    ),
                                    
-                                   #TODO: make all these conditional but use uioutput
-                                   sliderInput("year", label = "Year", min = min(year), max = max(year), value = c(2013, 2018), round = TRUE, step = 1),
-                                   selectInput("day_of_week", "Day of the week", day_of_week, selected = "..."),
-                                   selectInput("month", "Month", month, selected = "..."),
-                                   selectInput("weather", "Weather condition", weather_conditions, selected = "..."),
-                                   selectInput("driving_conditions", "Driving condition", driving_conditions, selected = "..."),
-                                   selectInput("road_condition", "Road condition", road_condition, selected = "..."),
-                                   selectInput("speed_limit", "Speed limit", speed_limit, selected = "..."),
-                                   selectInput("road_feature", "Road feature", road_feature, selected = "..."),
-                                   selectInput("crash_type", "Crash type", crash_type, selected = "..."),
-                                   selectInput("crash_severity", "Crash severity", crash_severity, selected = "...")
+                                   # checkboxGroupInput("check_box", "Activate filters", check_box),
+                                   checkboxInput('day_of_week_t', 'Day of the week', value = FALSE),
+                                   checkboxInput('month_t', 'Month', value = FALSE),
+                                   checkboxInput('Weather_t', 'Weather condition', value = FALSE),
+                                   checkboxInput('unit_t', 'Unit type involved', value = FALSE),
+                                   checkboxInput('driving_conditions_t', 'Driving conditions', value = FALSE),
+                                   checkboxInput('road_condition_t', 'Road conditions', value = FALSE),
+                                   checkboxInput('speed_limit_t', 'Speed limit', value = FALSE),
+                                   checkboxInput('road_feature_t', 'Road feature', value = FALSE),
+                                   checkboxInput('crash_type_t', 'Crash type', value = FALSE),
+                                   checkboxInput('crash_severity_t', 'Crash severity', value = FALSE),
+                                   
+                                   # as conditional panels
+                                   conditionalPanel(
+                                     condition = "input.day_of_week_t",
+                                     selectInput("day_of_week", "Day of the week", day_of_week, selected = "...")
+                                   ),
+                                   
+                                   conditionalPanel(
+                                     condition = "input.month_t",
+                                     selectInput("month", "Month", month, selected = "...")
+                                   ),
+                                   
+                                   conditionalPanel(
+                                     condition = "input.weather_t",
+                                     selectInput("weather", "Weather condition", weather_conditions, selected = "...")
+                                   ),
+                                   
+                                   conditionalPanel(
+                                     condition = "input.unit_t",
+                                     selectInput("unit", "Involving unit type", units, selected = "...")
+                                   ),
+                                   
+                                   conditionalPanel(
+                                     condition = "input.driving_conditions_t",
+                                     selectInput("driving_conditions", "Driving condition", driving_conditions, selected = "...")
+                                   ),
+                                   
+                                   conditionalPanel(
+                                     condition = "input.road_condition_t",
+                                     selectInput("road_condition", "Road condition", road_condition, selected = "...")
+                                   ),
+                                   
+                                   conditionalPanel(
+                                     condition = "input.speed_limit_t",
+                                     selectInput("speed_limit", "Speed limit", speed_limit, selected = "...")
+                                   ),
+                                   
+                                   conditionalPanel(
+                                     condition = "input.road_feature_t",
+                                     selectInput("road_feature", "Road feature", road_feature, selected = "...")
+                                   ),
+                                   
+                                   conditionalPanel(
+                                     condition = "input.crash_type_t",
+                                     selectInput("crash_type", "Crash type", crash_type, selected = "...")
+                                   ),
+                                   
+                                   conditionalPanel(
+                                     condition = "input.crash_severity_t",
+                                     selectInput("crash_severity", "Crash severity", crash_severity, selected = "...")
+                                   )
                           )
                         ),
                         
@@ -106,6 +164,21 @@ navbarPage("Road accidents in Queensland", id="road",
                         )
                     )
            ),
+           
+           tabPanel(
+             "Estimation",
+             sidebarPanel(
+               p(),
+               selectInput("floc", "Location", c("SA3" = "Loc_ABS_Statistical_Area_3", "SA4" = "Loc_ABS_Statistical_Area_4")),
+               selectInput("measure", "Measure", c("Fatality", "Cyclists", "Pedestrians")),
+               p(),
+               width = 2
+             ),
+            mainPanel(
+              plotOutput('forest_plot')
+              )
+           ),
+           
            tabPanel(
              "Data table",
              div(
@@ -115,6 +188,5 @@ navbarPage("Road accidents in Queensland", id="road",
                dataTableOutput("crash_data")
              )
            )
-           
-           #TODO: another tab with fatality rate estimates
 )
+
